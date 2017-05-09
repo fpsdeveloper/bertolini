0e38c4703ed84ea22c29acf4b6fb7e201ef97bdc
operador <operador>
2017-05-09 08:57:28 -0300

    Prueba de concepto

--- estudio/artcontrol/views.py
+++ estudio/artcontrol/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from models import caso
 
 def index(request):
     return render(request,'indice.html')
 
+#Listado Original de Casos
 def casos(request):
-    html = "<html><body>Listado Actual de Casos</body></html>"
-    return HttpResponse(html)
+    #Busco todos los casos en principio
+    casos = caso.objects.all()
+    context = {'casos':casos}
+    return render(request,'casos.html',context)
